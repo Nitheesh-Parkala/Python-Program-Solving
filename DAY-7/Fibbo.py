b40850b3dd65767df6705bdb3461dcd1d337b668
Nitheesh-Parkala <nitheeshprabhu2001@gmail.com>
2024-09-24 18:14:37 +0530

    Updated Fibonacci

--- DAY-7/Fibbo.py
+++ DAY-7/Fibbo.py
@@ -7,3 +7,21 @@ def checkFibbo(pos, n1, n2):
 pos = int(input("Enter the number of Fibonacci positions: "))
 res=checkFibbo(pos, 0, 1)
 
+#Fibonacci using Normal Function.
+
+def checkFibbo(pos,n1,n2):
+    for i in range(pos):
+        print(n1)
+        n1,n2=n2,n1+n2
+
+pos=int(input("Enter a Number "))
+checkFibbo(pos,0,1)
+
+#Fibonacci without function.
+
+pos=int(input("Enter a Number"))
+n1=0
+n2=1
+for i in range(pos):
+    print(n1)
+    n1,n2=n2,n1+n2
